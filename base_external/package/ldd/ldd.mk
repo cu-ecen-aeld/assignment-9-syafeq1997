36d6c18fec183319a291dde2724da34dc3ae2574
Syafiq Halim <syafeqhalim97@gmail.com>
2025-01-26 23:08:39 +0800

    Completed assignment 7 using buildroot

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,22 @@
+
+##############################################################
+#
+# LDD
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+LDD_VERSION = 'a08d7694a19e0cf34d644ea7d5ce2013d68a4452'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-syafeq1997.git'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = scull
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
