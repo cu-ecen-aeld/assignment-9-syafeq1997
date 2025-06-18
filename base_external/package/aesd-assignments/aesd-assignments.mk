ad6e69918dc3deede9096d10f9f20e48f31a10bb
Syafiq Halim <syafeqhalim97@gmail.com>
2025-06-18 23:17:07 +0800

    Assigment 8 completed

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'a73c9ba9387016942443cb6e8ca6621ff97e8eb9'
+AESD_ASSIGNMENTS_VERSION = '819f8f3276575ccadc5fe9232e76da88643dae69'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,9 +14,23 @@ AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignment-4-syafeq1997.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
+AESD_ASSIGNMENTS_LICENSE = NONE
+
+# Use local source directory instead of downloading
+#AESD_ASSIGNMENTS_OVERRIDE_SRCDIR = local path
+#AESD_ASSIGNMENTS_SITE = .
+#AESD_ASSIGNMENTS_SITE_METHOD = local
+#AESD_ASSIGNMENTS_SOURCE = dummy
+
+ifeq ($(BR2_PACKAGE_USE_AESD_CHAR_DEVICE),y)
+MY_CFLAGS += -DUSE_AESD_CHAR_DEVICE=1
+else
+MY_CFLAGS += -DUSE_AESD_CHAR_DEVICE=0
+endif
+
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all CROSS_COMPILE=aarch64-none-linux-gnu-
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all CROSS_COMPILE=aarch64-none-linux-gnu-
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) CFLAGS="$(MY_CFLAGS)" -C $(@D)/server all CROSS_COMPILE=aarch64-none-linux-gnu-
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -29,8 +43,18 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
 
-        $(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+    $(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+
+    $(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+    $(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
 endef
 
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver
+AESD_ASSIGNMENTS_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+
+$(eval $(kernel-module))
 $(eval $(generic-package))
